2bb6219df982dee26e8be8723778fd8fee6f052f
ildi-laszak <ildi.laszak@protonmail.com>
2022-07-15 08:13:25 +0200

    created BaseTest

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,53 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BaseTest {
+    protected WebDriver driver;
+    //protected HomePage homePage;
+    //protected String fixEmail = "testuser@test.com";
+
+    @BeforeAll
+    public void setup() throws IOException {
+        String browser;
+        InputStream inputStream = this.getClass().getResourceAsStream("test.properties");
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        browser = properties.getProperty("browser");
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            WebDriverManager.safaridriver().setup();
+            driver = new SafariDriver();
+        }
+        driver.manage().window().maximize();
+    }
+
+//    @BeforeEach
+//    public void loadHomePage() {
+//        homePage = new HomePage(driver);
+//        homePage.load();
+//    }
+
+    @AfterAll
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
