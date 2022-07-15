7a79734698c07e2b60198fc47b27322bd44a83ab
ildi-laszak <ildi.laszak@protonmail.com>
2022-07-15 09:00:47 +0200

    created TC01

---
+++ src/test/java/TC01_SuccessfulPageLoad.java
@@ -0,0 +1,16 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Feature("Homepage UI")
+public class TC01_SuccessfulPageLoad extends BaseTest{
+    @Test
+    @DisplayName("Successful page load")
+    @Description("The Homepage should load successfully")
+    public void successfulPageLoad(){
+        assertThat(homePage.getTitle()).isEqualTo("MEK (Magyar Elektronikus Könyvtár)");
+    }
+}
